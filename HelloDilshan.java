b8fbf91d2e4fe9ab950bd642fbfcbf41e909ea54
dilshann <dilshan.niroda@pearson.com>
2020-02-17 19:21:53 +0530

    initial commit

---
+++ HelloDilshan.java
@@ -0,0 +1,16 @@
+package com.pearson.nextgen.reportingservice;
+
+/**
+ * @author Dilshan Niroda
+ * @Date 2020-02-17
+ * @Time 7:18 PM
+ */
+public class HelloDilshan {
+
+    public static void main(String[] args) {
+
+        for (int i=0 ; i <10; i++)
+        System.out.println("Hello World "+i);
+    }
+
+}
